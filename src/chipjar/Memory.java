90640a648d8b1e8f6f5004640e6cafdbde16f4f2
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-25 20:16:26 +0200

    Chipjar: Rename m_memory to m_ram

--- src/chipjar/Memory.java
+++ src/chipjar/Memory.java
@@ -3,9 +3,9 @@ package chipjar;
 public class Memory {
 	protected final short RAM_SIZE = 4096;
 	
-	protected byte m_memory[];
+	protected byte m_ram[];
 
 	public Memory() {
-		this.m_memory = new byte[RAM_SIZE];
+		this.m_ram = new byte[RAM_SIZE];
 	}
 }
