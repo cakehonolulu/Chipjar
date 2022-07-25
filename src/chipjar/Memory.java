a1fa0f0770156658019d14bff2814b5538113c8b
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-25 20:37:46 +0200

    memory: Add memory read function (Returns byte)

--- src/chipjar/Memory.java
+++ src/chipjar/Memory.java
@@ -7,5 +7,15 @@ public class Memory {
 
 	public Memory() {
 		this.m_ram = new byte[RAM_SIZE];
+		
+		for (int i = 0; i < this.m_ram.length; i++)
+		{
+			this.m_ram[i] = 0;
+		}
 	}
+
+	protected byte m_memory_read(short m_where) {
+		return this.m_ram[m_where];
+	}
+	
 }
