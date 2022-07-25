ca2735b9251ba343f685b12ce979d890d415f7fa
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-25 20:12:09 +0200

    Chipjar: File loading mechanism for ROMs

--- src/chipjar/ChipjarApp.java
+++ src/chipjar/ChipjarApp.java
@@ -1,9 +1,64 @@
 package chipjar;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ChipjarApp {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Chipjar m_chipjar = new Chipjar();
+
+		// Used for file reading
+		InputStream m_file = null;
+
+		// Used for *actual* data reading
+		DataInputStream m_data = null;
+
+		byte[] m_rom_buffer;
+		
+		try {
+			
+			if (args.length == 0)
+			{
+				throw new Exception("No ROM Passed!");
+			}
+			
+			m_file = new FileInputStream(args[0]);
+
+			m_data = new DataInputStream(m_file);
+
+			if (m_data.available() <= (0xFFF - 0x200))
+			{
+				m_rom_buffer = new byte[m_data.available()];
+				m_data.readFully(m_rom_buffer);
+			}
+			else
+			{
+				throw new Exception("ROM Size too Large!");
+			}
+			
+			File m_romfile = new File(args[0]);
+			System.out.println("ROM Name: " + m_romfile.getName() + ", Size (Bytes): " + m_rom_buffer.length);
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+		}
+		finally
+		{
+			if (m_file != null)
+			{
+				m_file.close();
+			}
+
+			if (m_data != null)
+			{
+				m_data.close();
+			}
+		}
 
 	}
 
