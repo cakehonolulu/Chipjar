687829bcb756f3af88674a7a0e591071b2f39efd
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-26 12:55:26 +0200

    chipjar: Add graphics subsystem to the constructor

--- src/chipjar/Chipjar.java
+++ src/chipjar/Chipjar.java
@@ -3,9 +3,11 @@ package chipjar;
 public class Chipjar {
 	public Cpu m_cpu;
 	public Memory m_memory;
+	public Graphics m_graphics;
 	
 	public Chipjar() {
 		this.m_cpu = new Cpu();
 		this.m_memory = new Memory();
+		this.m_graphics = new Graphics();
 	}
 }
