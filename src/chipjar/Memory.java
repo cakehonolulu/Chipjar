a1ace87334b917fe0f390d037e3e6ee7c6ad0f01
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-26 12:59:33 +0200

    memory: Use 'Arrays' to set the entire array to 0

--- src/chipjar/Memory.java
+++ src/chipjar/Memory.java
@@ -1,5 +1,7 @@
 package chipjar;
 
+import java.util.Arrays;
+
 public class Memory {
 	protected final short RAM_SIZE = 4096;
 	
@@ -8,10 +10,7 @@ public class Memory {
 	public Memory() {
 		this.m_ram = new byte[RAM_SIZE];
 		
-		for (int i = 0; i < this.m_ram.length; i++)
-		{
-			this.m_ram[i] = 0;
-		}
+		Arrays.fill(m_ram, (byte) 0);
 	}
 
 	protected byte m_memory_read(short m_where) {
